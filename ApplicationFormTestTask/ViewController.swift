c127bb04d8848ff0b1c0dd9bbcc2f9cec2f0ce74
Ksenia Kobak <79209115+Ksenia-Ksu@users.noreply.github.com>
2022-10-24 14:01:38 +0300

    Initial Commit

---
+++ ApplicationFormTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApplicationFormTestTask
+//
+//  Created by Ксения Кобак on 24.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
